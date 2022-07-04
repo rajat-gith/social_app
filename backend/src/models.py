7102fcd5cbb6cf05a5e5dafee135a23d9ef0aa5a
Gourav Das <gourav2001das@gmail.com>
2022-07-04 14:40:57 +0530

    Added the functionality for comment creation

--- backend/src/models.py
+++ backend/src/models.py
@@ -17,6 +17,7 @@ class Post(models.Model):
     content = models.TextField()
     creator=models.CharField(max_length=20)
     created_on = models.DateTimeField(auto_now_add=True)
+    comment = models.ForeignKey("Comment",related_name='post_comments',on_delete=models.SET_NULL,null = True)
     _id=models.AutoField(primary_key=True,editable=False)
     #image=models.ImageField(null=True,blank=True)
 
@@ -27,7 +28,7 @@ class Post(models.Model):
         return self.title
     
 class Comment(models.Model):
-    post=models.ForeignKey(Post,on_delete=models.SET_NULL,null=True)
+    post=models.ForeignKey(Post,related_name ='comment_post_detail' ,on_delete =models.CASCADE,null=True)
     user= models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=80)
     email = models.EmailField()
@@ -36,3 +37,8 @@ class Comment(models.Model):
 
     def __str__(self):
         return str(self.body)
+    
+    def save(self,*args,**kwargs):
+        super(Comment,self).save(*args,**kwargs)
+        curr_post =self.post
+        curr_post.comment.add(self)
